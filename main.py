d619b8805180ebcd66212ce84a68a617531aa72e
JXSS10 <mrhetlr98@gmail.com>
2025-04-01 18:41:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import yt_dlp
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import MessageNotModified
-from config import API_ID, API_HASH, BOT_TOKEN, DOWNLOAD_LOCATION, SESSION_NAME # استيراد المتغيرات من config.py
+from plugins.config import API_ID, API_HASH, BOT_TOKEN, DOWNLOAD_LOCATION, SESSION_NAME # استيراد المتغيرات من config.py
 
 # --- بيانات البوت (تم استيرادها من config.py) ---
 # API_ID = ваш_api_id  # استبدل بـ API ID الخاص بك -  **الآن مستورد من config.py**
